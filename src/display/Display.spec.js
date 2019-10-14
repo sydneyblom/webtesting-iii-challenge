6b751b0de79f4df6819bb5f8034ec7d41834e4aa
Sydney Blom <sydneyblom@#gmail.com>
2019-10-14 15:44:35 -0500

    display tests

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,31 @@
-// Test away!
+import React from "react";
+import { render } from "@testing-library/react";
+import Display from "../display/Display";
+
+test("Display should match snapshot", () => {
+  expect(render(<Display />)).toMatchSnapshot();
+});
+
+//red
+test("should display red when locked", () => {
+  const { getByText } = render(<Display closed={true} locked={true} />);
+  getByText(/locked/i);
+});
+
+//green unlocked
+test("should display green when unlocked", () => {
+  const { getByText } = render(<Display closed={true} locked={false} />);
+  getByText(/unlocked/i);
+});
+
+//red closed
+test("should display red when closed", () => {
+  const { getByText } = render(<Display closed={true} locked={false} />);
+  expect(getByText(/closed/i).classList.contains("red-led")).toBe(true);
+});
+
+//green closed
+test("should display green when open", () => {
+  const { getByText } = render(<Display closed={false} locked={false} />);
+  expect(getByText(/open/i).classList.contains("green-led")).toBe(true);
+});
